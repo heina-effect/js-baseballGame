ed908b6916a8fba82565c2974127472d6503b585
Heina <heina2ffect@gmail.com>
2022-02-21 15:27:16 +0900

    fix: 힌트 및 승리 메세지 출력 처리

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let ran = 345; //랜덤 선택으로 변경필요
+let input = document.body.querySelector('input') //입력값
+let resetBtn = document.body.querySelector(".reset");
+let result = document.body.querySelector(".result"); //결과도출
 
+let ran = 345; //랜덤 선택으로 변경필요
 let strike = 0;
 let ball = 0;
-
-let answer= 345; //input으로 값 받을 예정
+let answer= 346; //input으로 값 받을 예정
 
 function game (){
     ran = ran.toString().split('')
@@ -17,7 +19,15 @@ function game (){
         }
     }
     
+    if(strike === 3){
+        result.innerText = '정답입니다! 게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.';
+    }
+    else{
+        result.innerText = '스트라이크' + strike + ', 볼' + ball;
+        input.value = '';
+        input.focus();
+    }
 }
 game()
 
-console.log('스트라이크' + strike + ', 볼' + ball);
+
